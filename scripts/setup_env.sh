a8566a15e31183c98d0c951f26ee879251c13548
Bryce Thuilot <bthuilot@gmail.com>
2020-06-17 12:23:48 -0400

    Trying new permissioning

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -5,3 +5,14 @@ if [ -d /var/www/BoozeBetter ]; then
     rm -rf /var/www/BoozeBetter
 fi
 mkdir -vp /var/www/BoozeBetter
+
+#!/bin/bash
+# Set ownership for all folders and files
+chown -R ec2-user:ec2-user /var/www/
+chown -R root:root /var/www/protected
+
+# set files to 644 
+find /var/www/ -type f -print0 | xargs -0 chmod 0644
+
+# set folders to 755
+find /var/www/ -type d -print0 | xargs -0 chmod 0755
