62c20b05606eda155db2bfb7a098cd52c2a03875
Bryce Thuilot <bthuilot@gmail.com>
2020-06-17 12:00:56 -0400

    Fix scripts

--- scripts/migrate_db.sh
+++ scripts/migrate_db.sh
@@ -1,6 +1,6 @@
 # Source DB Enviorment variables
 source /opt/boozebetter/postgres-db.sh
-export DATABASE_URL=postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASEh
+export DATABASE_URL=postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}
 
 pushd /var/www/BoozeBetter/src
 yarn migrate up
